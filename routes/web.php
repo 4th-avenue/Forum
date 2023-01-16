8deb878ad7dd2464bf07492fb2e75ef030ab2c85
mystee <quebec706@gmail.com>
2023-01-16 20:31:16 +0900

    포럼 컨트롤러 생성

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ForumController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
