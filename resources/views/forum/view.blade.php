0340a23db36ffbe2174ba728112dd5bdb51e387a
mystee <quebec706@gmail.com>
2023-01-17 15:37:16 +0900

    view 페이지 수정

--- resources/views/forum/view.blade.php
+++ resources/views/forum/view.blade.php
@@ -13,10 +13,33 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row my-3">
             <div class="col-12">
-                <div class="d-grid gap-2 col-6 mx-auto">
-                    <button class="btn btn-primary" type="button">Button</button>
+                <div class="d-grid gap-2 col-3 mx-auto">
+                    <button class="btn btn-outline-danger rounded-pill fs-4"><i class="fa fa-heart" aria-hidden="true"></i> 3</button>
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-12">
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-action">A second item</li>
+                    <li class="list-group-item list-group-item-action">A third item</li>
+                    <li class="list-group-item list-group-item-action">A fourth item</li>
+                    <li class="list-group-item list-group-item-action">And a fifth one</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="row my-3">
+            <div class="col-12">
+                <input type="text" class="form-control">
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-12">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-primary" type="button">Submit</button>
                 </div>
             </div>
         </div>
