e5077d3dd829aa5d3d59105b06a26ad5a1ebaad6
mystee <quebec706@gmail.com>
2023-01-17 20:04:49 +0900

    카테고리 기능 구현중

--- resources/views/category/view.blade.php
+++ resources/views/category/view.blade.php
@@ -4,9 +4,13 @@
     <div class="container">
         <div class="row my-3">
             <div class="col-12">
-                <input type="text" class="form-control" value="Music">
+                <input type="text" class="form-control">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3">
+                    <button class="btn btn-primary" type="button">Create</button>
+                </div>
             </div>
         </div>
+        <hr>
         <div class="row my-3">
             <div class="col-12">
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
