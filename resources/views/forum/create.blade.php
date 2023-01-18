693095d30b0087b992b438d95d0cd5b93828fc48
mystee <quebec706@gmail.com>
2023-01-18 15:39:40 +0900

    category 테이블 생성

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -14,7 +14,23 @@
         </div>
         <div class="row my-3">
             <div class="col-12">
+                <label>Category</label>
+                <select class="form-select">
+                    <option value="music">music</option>
+                    <option value="movie">movie</option>
+                </select>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-12">
                 <div id="editor"></div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-12">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3">
+                    <button class="btn btn-success" type="button">Submit</button>
+                </div>
             </div>
         </div>
     </div>
