9db2e9fcbddd8284e8eca9835072f409d8e20d04
mystee <quebec706@gmail.com>
2023-01-19 16:12:05 +0900

    category 수정 기능 구현

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,12 +13,14 @@ class CategoryController extends Controller
 
         return view('category.index')->with('categories', $categories);
     }
+
     public function view($id)
     {
         $category = Category::find($id);
 
         return view('category.view')->with('category', $category);;
     }
+
     public function store(Request $request)
     {
         $category = new Category;
@@ -27,11 +29,21 @@ class CategoryController extends Controller
 
         return redirect('/category');
     }
+
     public function delete($id)
     {
         $category = Category::find($id);
         $category->delete();
-        
+
+        return redirect('/category');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->title = $request->title;
+        $category->save();
+
         return redirect('/category');
     }
 }
