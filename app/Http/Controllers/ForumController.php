ce22b56c45fb5a73131ec413a4c8c186431e8fd1
mystee <quebec706@gmail.com>
2023-01-19 21:39:27 +0900

    CKEditor로 작성한 글 DB로 보내기 성공

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Post;
 
 class ForumController extends Controller
 {
@@ -19,5 +20,21 @@ class ForumController extends Controller
     {
         $categories = Category::orderby('title', 'asc')->get();
         return view('forum.create')->with('categories', $categories);
+    }
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->category_id = $request->category_id;
+        $post->content = $request->content;
+        $post->save();
+
+        $result = $request->all();
+
+        $data = array(
+            'result' => $result
+        );
+
+        return response()->json($data);
     }
 }
--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -9,13 +9,13 @@
         <div class="row mt-5">
             <div class="col-12">
                 <label>Title</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" id="title">
             </div>
         </div>
         <div class="row my-3">
             <div class="col-12">
                 <label>Category</label>
-                <select class="form-select">
+                <select class="form-select" id="category_id">
                     @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->title}}</option>
                     @endforeach
@@ -30,7 +30,7 @@
         <div class="row my-3">
             <div class="col-12">
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3">
-                    <button class="btn btn-success" type="button">Submit</button>
+                    <button class="btn btn-success" type="button" id="submit">Submit</button>
                 </div>
             </div>
         </div>
@@ -45,5 +45,36 @@
         .catch( error => {
             console.error( error );
         } );
+</script>
+
+<script>
+    var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+    $( document ).ready(function() {
+        $('#submit').click(function() {
+            var title = $('#title').val();
+            var category_id = $('#category_id').val();
+            var content = $('.ck-content').html();
+            
+            $.ajax({
+                type: "POST",
+                url: "/store",
+                data: {
+                    _token: CSRF_TOKEN,
+                    title: title,
+                    category_id: category_id,
+                    content: content,
+                },
+                dataType: 'JSON',
+                success: function success(data) {
+                    console.log(data.result);
+                    window.location.href = '/';
+                },
+                error: function (response) {
+                    console.log(response);
+                }
+            });
+        });
+    });
 </script>
 @endsection
