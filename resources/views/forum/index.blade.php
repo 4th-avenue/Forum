ddf087334628cfffe392c557513bab28f78128c4
mystee <quebec706@gmail.com>
2023-01-20 15:07:35 +0900

    포럼 index 페이지 DB 연동

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -12,31 +12,38 @@
         <hr>
 
         @php
-            $categories = App\Models\category::orderby('title', 'asc')->get();
+            $categories = App\Models\Category::orderby('title', 'asc')->get();
         @endphp
         @if(count($categories)>0)
-        @foreach ($categories as $category)
-        <div class="row mt-3">
-            <div class="col-12">
-                <h4>{{$category->title}}</h4>
-                <ul class="list-group">
-                    <li class="list-group-item list-group-item-action">
-                        <a href="{{url('/')}}/1/view">Three Days of the Condor</a>
-                        <span class="badge rounded-pill bg-info"><i class="fa fa-comment" aria-hidden="true"></i> 3</span>
-                        <span class="badge rounded-pill bg-danger"><i class="fa fa-heart" aria-hidden="true"></i> 3</span>
-                        <br>
-                        <small>2023-01-16 | by mystee</small>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-12 my-3">
-                <div class="d-grid gap-2 col-6 mx-auto">
-                    <button type="button" class="btn btn-dark">All Posts of Movie Category</button>
-                </div>
-            </div>
-        </div>
-        <hr>
-        @endforeach
+            @foreach ($categories as $category)
+                @php
+                    $posts = App\Models\Post::where('category_id', $category->id)->orderby('id', 'desc')->limit(3)->get();
+                @endphp
+                @if(count($posts)>0)
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <h4>{{$category->title}}</h4>
+                            <ul class="list-group">
+                                @foreach ($posts as $post)
+                                <li class="list-group-item list-group-item-action">
+                                    <a href="{{url('/')}}/1/view" style="text-decoration: none">{{$post->title}}</a>
+                                    <span class="badge rounded-pill bg-info"><i class="fa fa-comment" aria-hidden="true"></i> 3</span>
+                                    <span class="badge rounded-pill bg-danger"><i class="fa fa-heart" aria-hidden="true"></i> 3</span>
+                                    <br>
+                                    <small>{{$category->created_at}}・by mystee</small>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="col-12 my-3">
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button type="button" class="btn btn-dark">All Posts of {{$category->title}} Category</button>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                @endif
+            @endforeach
         @endif
     </div>
 @endsection
